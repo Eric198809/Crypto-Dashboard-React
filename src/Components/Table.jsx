47b835deebc6b7db485b350770e9dd7075820e4c
Eric Besson <150351199+Eric198809@users.noreply.github.com>
2024-12-13 17:11:39 +0100

    Lignes table ok

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,18 +1,71 @@
 import { useState } from "react";
-
+import Tableline from "./Tableline";
 
 const Table = ({ coinsData }) => {
-const[rangeNumber, setRangeNumber]= useState(100) 
+  const [rangeNumber, setRangeNumber] = useState(100);
+  const [orderBy, setOrderBy] = useState("");
+  const tableHeader = [
+    "Prix",
+    "MarketCap",
+    "Volume",
+    "1h",
+    "1j",
+    "1s",
+    "1m",
+    "6m",
+    "1a",
+    "ATH",
+  ];
 
   return (
-<div className="table-container">
-<div className="table-header">
-  <div className="range-container">
-    <span> Top {" "} <input type="text" value={rangeNumber} onChange={(e)=> setRangeNumber(e.target.value)}/></span>
-    <input type="range" min= "1" max="250" value={rangeNumber} onChange={(e)=> setRangeNumber(e.target.value)}/>
-  </div>
-</div>
-</div>
+    <div className="table-container">
+      <ul className="table-header">
+        <div className="range-container">
+          <span>
+            {" "}
+            Top{" "}
+            <input
+              type="text"
+              value={rangeNumber}
+              onChange={(e) => setRangeNumber(e.target.value)}
+            />
+          </span>
+          <input
+            type="range"
+            min="1"
+            max="250"
+            value={rangeNumber}
+            onChange={(e) => setRangeNumber(e.target.value)}
+          />
+        </div>
+        {tableHeader.map((element) => (
+          <li key={element}>
+            <input
+              type="radio"
+              name="header-element"
+              id={element}
+              defaultChecked={
+                element === orderBy || element === `${orderBy} reverse`
+                  ? true
+                  : false
+              }
+              onClick={() => {
+                if (orderBy === element) {
+                  setOrderBy(`${element} reverse`);
+                } else {
+                  setOrderBy(element);
+                }
+              }}
+            />
+            <label htmlFor={element}>{element}</label>
+          </li>
+        ))}
+      </ul>
+      {coinsData &&
+        coinsData
+          .slice(0, rangeNumber)
+          .map((coin , index) => <Tableline key={index} coin={coin} index= {index}/>)}
+    </div>
   );
 };
 
